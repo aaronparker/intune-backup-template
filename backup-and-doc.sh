007645d66eb2603c2f71b75e2325eb4f2a5fe3b7
Aaron Parker <aaron@stealthpuppy.com>
2023-05-09 11:43:09 +1000

    Update backup-and-doc.sh
    Add 'TENANT=$(jq .params.TENANT_NAME $PWD/auth.json | tr -d \")'

--- backup-and-doc.sh
+++ backup-and-doc.sh
@@ -3,8 +3,9 @@ IntuneCD-startbackup --mode=1 --output=json --path="$PWD/prod-backup" --localaut
     #--exclude=assignments
 
 # Generate the as-built document in markdown
+TENANT=$(jq .params.TENANT_NAME $PWD/auth.json | tr -d \")
 INTRO="Endpoint Manager backup and documentation generated locally. <img align=\"right\" width=\"96\" height=\"96\" src=\"./logo.png\">"
-IntuneCD-startdocumentation --path="$PWD/prod-backup" --outpath="$PWD/prod-as-built.md" --tenantname="stealthpuppylab.onmicrosoft.com" --intro=$INTRO
+IntuneCD-startdocumentation --path="$PWD/prod-backup" --outpath="$PWD/prod-as-built.md" --tenantname=$TENANT --intro=$INTRO
 
 # Generate a PDF document from the as-built markdown
 md-to-pdf "$PWD/prod-as-built.md" --config-file ./md2pdf/pdfconfig.json
